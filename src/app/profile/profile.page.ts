746f603519bd803f53ea1d681a96b18c93e9ae95
fercho97 <fernando.jaime@alumnos.udg.mx>
2019-11-19 22:20:01 -0600

    feat(edición usuario) se agrego la funcionalidad de edición de datos de usuario, así como el inicio de las vistas para el historial

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from './profile.service';
 import { ToastrService } from 'ngx-toastr';
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -8,13 +10,15 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./profile.page.scss'],
   providers: [ProfileService]
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage{
 
   id = window.localStorage.getItem('id');
+  private values : HttpParams;
   usuario = {} as any;
+  soloVista : boolean = true;
   constructor(private profileServ : ProfileService, private toast : ToastrService) { }
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.profileServ.getUser(this.id).subscribe( (res: any) =>{
       this.usuario = res.body;
       console.log(this.usuario);
@@ -26,5 +30,33 @@ export class ProfilePage implements OnInit {
 
   underConstruction(){
     this.toast.warning('Vista y funcionalidad en construcción', 'En proceso');
+  }
+
+  comenzarEdicion(){
+    this.soloVista=false;
+  }
+
+  cancelar(){
+    this.soloVista=true;
+  }
+
+  actualizarDatos(form){
+    if(form.value.nickname=="" || form.value.nombres=="" || form.value.apellidos==""){
+      this.toast.error("No debe dejar ningun campo vacio",'Campo vacio');
+    }
+    else{
+        this.values = new HttpParams()
+        .set('nickname', form.value.nickname)
+        .set('nombres', form.value.nombres)
+        .set('apellidos', form.value.apellidos)
+        this.profileServ.updateUser(this.id, this.values).subscribe( (res: any) =>{
+          this.soloVista=true;
+          this.toast.success('Datos Modificados con éxito', 'Modificación Exitosa!');
+        },
+      error =>{
+        console.log(error.message);
+          this.toast.error(error.error.message,'Error inesperado');
+      })
+    }
   }
 }
