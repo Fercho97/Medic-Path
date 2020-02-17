1aa4737171c39f2bdf1c357f3c632a17ee155997
fercho97 <fernando.jaime@alumnos.udg.mx>
2020-02-16 19:51:34 -0600

    Logout alert

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { LoginService } from '../login/login.service';
 import {Router} from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-landing',
@@ -12,7 +13,7 @@ export class LandingPage {
   username : string = "";
   isDoctor : boolean = false;
 
-  constructor(private toast : ToastrService, private logServ : LoginService, private router : Router) { }
+  constructor(private toast : ToastrService, private logServ : LoginService, private router : Router, private alertCtr : AlertController) { }
   ionViewWillEnter(){
     console.log(window.localStorage.getItem('username'));
     console.log(window.localStorage.getItem('token'));
@@ -29,13 +30,38 @@ export class LandingPage {
 
   logout(){
 
-    this.logServ.logout(window.localStorage.getItem('token')).subscribe( (res: any) =>{
-      window.localStorage.clear();
-      this.router.navigate([''])
-    },
-  error =>{
-      console.log(error);
-  })
+    this.logoutAlert();
+
+    
+  }
+
+  async logoutAlert(){
+    const alert = await this.alertCtr.create({
+      header: 'Logout',
+      message: '¿Desea salir de la aplicación?',
+      buttons : [{
+        text: 'No',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: () =>{
+
+        }
+      },{
+        text: 'Si',
+        handler: () =>{
+          this.logServ.logout(window.localStorage.getItem('token')).subscribe( (res: any) =>{
+            window.localStorage.clear();
+            this.router.navigate([''])
+          },
+        error =>{
+            console.log(error);
+        })
+        }
+      }
+    ]
+    });
+
+    await alert.present();
   }
 
 }
